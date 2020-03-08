4e3087f4b3e8397048d33c1ddf401792ffa3ff4b
Rishabh <rishabhmalhotra091@gmail.com>
2020-03-09 00:52:33 +0530

    added first domain

---
+++ src/main/java/org/webScrapper/App.java
@@ -0,0 +1,9 @@
+package org.webScrapper;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
---
+++ src/main/java/org/webScrapper/domains/Mobiles.java
@@ -0,0 +1,68 @@
+package org.webScrapper.domains;
+
+public class Mobiles {
+
+    private String name;
+    private double price;
+    private String brand;
+    private String details;
+    private String model;
+    private String imgUrl;
+    private String category;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
